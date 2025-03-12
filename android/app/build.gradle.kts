0bf924d73e8bd0d3fbad056586d0b688450c67e4
Basym Bhamani <bbhamani@uoguelph.ca>
2025-03-11 21:13:07 -0400

    Attempted fix to run in android emulator

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 android {
     namespace = "com.example.the_great_ape_escape"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flame/game.dart';
 import 'game.dart';
 
 void main() {
-  runApp(GameWidget(game: ApeEscapeGame()));
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.landscapeLeft,
+    DeviceOrientation.landscapeRight,
+  ]).then((_) {
+    runApp(
+      MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+          body: GameWidget(game: ApeEscapeGame()), // Correct way to embed a Flame game
+        ),
+      ),
+    );
+  });
 }
